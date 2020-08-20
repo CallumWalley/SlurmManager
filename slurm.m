979e426bda15281f25757ec27d2bc45a0df942e0
Callum Walley <callum.walley@nesi.org.nz>
2020-08-20 13:47:23 +1200

    first test

--- slurm.m
+++ slurm.m
@@ -3,64 +3,77 @@ classdef slurm
     %   Detailed explanation goes here
     
     properties
-        account
-        cpus_per_task=1
-        mem=1500
-        time=duration(0,15,0)
-        workspace='.matlab_slurm'
-        qos='debug'
+        account;
+        cpus_per_task=1;
+        mem=1500;
+        time=duration(0,15,0);
+        pass_workspace=0;
+        workspace='.matlab_slurm';
+        debug=0;
+        qos='debug';
     end
     
     
     
     methods
         function sfor(obj, functionHandle, inputArray)
-            mkdir(obj.workspace);
+
+            % TODO: Validate funtion.
+            %       Allow multi-input funtions.
+            if obj.debug, properties(obj), end
+
             workspacename = fullfile(obj.workspace, 'workspace.mat');
-            outputname = fullfile(obj.workspace, '\${SLURM_ARRAY_TASK_ID}.mat');
+            handlename = fullfile(obj.workspace, 'handle.m');
+            mkdir(obj.workspace);
+
             save(workspacename);
-            range = strcat('1-',string(length(inputArray)));
+            save(handlename,'functionHandle');
+            mtlbcmd = ['disp(starting MATLAB call);'];
             
-            mtlbcmd = [strcat("load('", workspacename,"');"),...
-                "functionHandle(inputArray(:,\${SLURM_ARRAY_TASK_ID}));",...
-                "save(", strcat("'", outputname, "'"),")"];
-            disp(strjoin(mtlbcmd, ' '));
+            if obj.pass_workspace
+                mtlbcmd=[mtlbcmd, "load('", workspacename,"');"];
+            end
+            mtlbcmd=[mtlbcmd, "load('", handlename,"');",...
+                "functionHandle(\${SLURM_ARRAY_TASK_ID});",...
+                "disp(starting MATLAB call);"];
+            if obj.debug, disp(strjoin(['MATLAB CMD: ',mtlbcmd], '')), end
+
             % Construct slurm job.
-            bshcmd=['matlab', '-nodisplay', '-r',...
-                strcat('\"', strjoin(mtlbcmd,' '), '\"')];
-            disp(strjoin(bshcmd, ' '));
+            bshcmd=['matlab -nodisplay -r', mtlbcmd];
+            if obj.debug, disp(strjoin(['BASH CMD: ',bshcmd], '')), end
+                
             cmd = ['sbatch',...
-                '--job-name', 'test%x',...
-                '--cpus-per-task', string(obj.cpus_per_task),...
-                '--mem', string(obj.mem),...
-                '--output', fullfile(obj.workspace,'all.log'),...
-                '--open-mode', 'append',...
-                '--time', char(obj.time),...
-                '--array', range,...
-                '--wrap', strcat('"',strjoin(bshcmd,' '),'"')];
+                ' --job-name ', 'test%x',...
+                ' --cpus-per-task ', string(obj.cpus_per_task),...
+                ' --mem ', string(obj.mem),...
+                ' --output ', fullfile(obj.workspace,'all.log'),...
+                ' --open-mode ', 'append',...
+                ' --time ', char(obj.time),...
+                ' --array ', strjoin(inputArray,',')];
             
-            disp(strjoin(cmd, ' '));
-            %
-            [submitstatus jobidstring]=system(strjoin(cmd, ' '));
+                if exist('obj.account','var')
+                    cmd = [cmd, ' --account ', obj.account];
+                end
+                if exist('obj.qos','var')
+                    cmd = [cmd, ' --qos ', obj.qos];
+                end
+                cmd = [cmd, '--wrap "',  bshcmd, '"'];
+                
+            
+            if obj.debug, disp(strjoin(['FULL CMD: ',cmd], '')), end
+
+            [submitstatus, jobidstring]=system(strjoin(cmd, ''));
             if submitstatus
                 error(jobidstring);
             end
             jobidarray=split(jobidstring, ' ');
             jobid=strtrim(jobidarray(4));
             chaser_cmd = ['srun','--job-name','--qos', obj.qos, 'chaser','--dependency', (strcat('after:',jobid)), '--time','00:00:01', 'true'];
-            disp(strjoin(chaser_cmd, ' '));
-            [submitstatus jobidstring]=system(strjoin(chaser_cmd, ' '));
+            if obj.debug, disp(strjoin(['CHASER CMD: ', cmd], '')), end
+            [submitstatus, jobidstring]=system(strjoin(chaser_cmd, ' '));
             if submitstatus
                 error(jobidstring);
             end
-            
-            for x=1:length(inputArray)
-                load(fullfile(obj.workspace, strcat(string(x), '.mat')));
-            end
-            %             syste
-            %system(cmd)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
         end
     end
 end
