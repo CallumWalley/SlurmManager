bbbefac3711fa41fd89fcd7d54d6c29ed8eb319f
Callum Walley <callum.walley@nesi.org.nz>
2020-08-20 20:07:28 +1200

    testfile

--- test.m
+++ test.m
@@ -1,10 +1,11 @@
-inputs={[1,1],[1,2],[5,10],[20,2]};
-%sc=slurm();
-thing=@timesten;
-thing(inputs(:,1));
+sc = slurm();
+%sc.debug = 1;
+sc.arrayMax = 2;
+sc.sfor(@exampleFunction, 1:12);
 
-function output = timesten(input1, input2)
-    output(index) = input * input2;
+function exampleFunction(input)
+    disp(input);
+    pause(randi([1,100],1));
 end
 
 
